699859cad59417fddc8462a81212c57ff71f0af5
Fbartoli <florent.bartoli@sygnum.com>
2024-03-21 13:43:31 +0100

    first commit

---
+++ src.js
@@ -0,0 +1,11 @@
+const { HDNodeWallet } = require("ethers")
+require('dotenv').config()
+
+const phrase = process.env.phrase
+console.log(phrase)
+const limit = 20
+
+for (let index = 0; index < limit; index++) {
+    const wallet = HDNodeWallet.fromPhrase(phrase, '', `m/44'/60'/${index}'/0/0`)
+    console.log(wallet.address)
+}
