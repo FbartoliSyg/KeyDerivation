c60b2d161ae587e36b5d13fb99332ffce276ce8a
Fbartoli <florent.bartoli@sygnum.com>
2024-03-21 14:02:26 +0100

    metamask path

--- src.js
+++ src.js
@@ -5,6 +5,6 @@ const phrase = process.env.phrase
 const limit = 20
 
 for (let index = 0; index < limit; index++) {
-    const wallet = HDNodeWallet.fromPhrase(phrase, '', `m/44'/60'/${index}'/0/0`)
+    const wallet = HDNodeWallet.fromPhrase(phrase, '', `m/44'/60'/0'/0/${index}`)
     console.log(wallet.address)
 }
