a1244e4941517bac02473ab58f50ac1f765405cd
Fbartoli <florent.bartoli@sygnum.com>
2024-03-21 14:14:52 +0100

    increase addresses

--- src.js
+++ src.js
@@ -2,8 +2,7 @@ const { HDNodeWallet } = require("ethers")
 require('dotenv').config()
 
 const phrase = process.env.phrase
-const limit = 20
-
+const limit = 60
 for (let index = 0; index < limit; index++) {
     const wallet = HDNodeWallet.fromPhrase(phrase, '', `m/44'/60'/0'/0/${index}`)
     console.log(wallet.address)
